a3233453ca541329c13f2bc765e16d566552464f
arun <cvaruncv1998@gmail.com>
2022-11-03 11:47:21 +0530

    create post modal completed

--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -3,7 +3,9 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
-import { TextField } from '@mui/material';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
+
 
 
 const style = {
@@ -13,11 +15,12 @@ const style = {
   transform: 'translate(-50%, -50%)',
   width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  borderRadius:'10px' ,
   boxShadow: 24,
   pt: 2,
   px: 4,
   pb: 3,
+  shadow:''
 };
 
 
@@ -42,7 +45,7 @@ export default function NestedModal(props) {
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
       >
-        <Box sx={{ ...style, width: 400 }}>
+        <Box sx={{ ...style, width: 400}}>
           <div className='h-80'>
             <div className='flex '><h2 className='text-2xl text-right w-4/6'>Create Post</h2>
               <span className='w-2/6 flex flex-row-reverse'><CloseIcon onClick={handleClose} /></span></div>
@@ -52,7 +55,7 @@ export default function NestedModal(props) {
               <img className=' max-h-12 my-auto' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkSQpuRwKcQD_-_2yf6EGsw56SsFVa4jdKaQ&usqp=CAU' alt='profile' />
 
               <div className='m-3'>
-                <h3 className='m-1 text-lg '>Arun cv</h3>
+                <h3 className='m-1 text-lg '>Name</h3>
                 <select className='bottom-2 rounded-sm border-gray-400 hover:text-lg'>
                   <option>Friends</option>
                   <option >Public</option>
@@ -60,9 +63,16 @@ export default function NestedModal(props) {
               </div>
 
             </div>
-            <div className='flex flex-col '>
+            <div className='flex flex-col  justify-between  h-[220px]'>
               <input placeholder='Whats on your Mind?' className='border-0 w-full mr-5 p-4' />
-              <button>Post</button>
+              <div className='mb-3'>
+                <div className='w-full my-3 h-[60px] rounded-md shadow-md ring-2 ring-slate-200 flex items-center justify-between px-4'>
+                  <p>Add to your Post</p>
+                  <AddPhotoAlternateIcon />
+                  <AddReactionIcon />
+                </div>
+                <Button variant='contained' sx={{ width: '100%' }}>Post</Button>
+              </div>
 
             </div>
           </div>
